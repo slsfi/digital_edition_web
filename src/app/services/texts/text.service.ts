074a939dc39cc212a92262cd9d833e22d5cce22c
Hannes Dahlvik <43564378+HannesDahl@users.noreply.github.com>
2020-04-16 10:36:51 +0300

    Parse and fetch all illustrations

--- src/app/services/texts/text.service.ts
+++ src/app/services/texts/text.service.ts
@@ -22,7 +22,6 @@ export class TextService {
 
 
   getEstablishedText(id: string): Observable<any> {
-    console.log(id);
     const id2 = id.replace('_est', '');
     const parts = id2.split(';');
     const c_id = `${id}`.split('_')[0];
@@ -41,10 +40,6 @@ export class TextService {
             return this.cache.getHtml(id);
           })
           .catch(this.handleError);
-  }
-
-  getEstablishedImages(id: string) {
-    console.log(this.getEstablishedText('167_13642'));
   }
 
   getIntroduction(id: string, lang: string): Observable<any> {
--- src/components/illustrations/illustrations.ts
+++ src/components/illustrations/illustrations.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavParams } from 'ionic-angular';
-import { ConfigService } from '@ngx-config/core';
-import { HttpClient } from '@angular/common/http';
 import { TextService } from '../../app/services/texts/text.service';
 /**
  * Generated class for the IllustrationsComponent component.
@@ -14,40 +12,26 @@ import { TextService } from '../../app/services/texts/text.service';
   templateUrl: 'illustrations.html'
 })
 export class IllustrationsComponent {
-  text: string;
+  @Input() itemId: string;
   illustrationsPath = 'assets/images/illustrations/2/';
   imgPath: any;
-  image: any[] = [
-    { src: 'https://picsum.photos/id/237/200/300', id: ''}
-  ];
+  images: any = [];
+  viewAll = false;
   constructor(
     public navParams: NavParams,
-    private config: ConfigService,
-    private textService: TextService,
-    private _http: HttpClient
-  ) {
-    this.text = 'Hello World';
-  }
+    private textService: TextService
+  ) { }
   ngOnInit() {
-    console.log(this.navParams);
-    console.log(this.config.getSettings('app.apiEndpoint') + '/');
-
-    console.log(this.textService);
-
-    const api = this.config.getSettings().app.apiEndpoint;
-    const url = `${api}/topelius/media/image/metadata//173`;
-
-    this._http.get(url).subscribe((data) => {
-      console.log(data);
-    })
+    this.textService.getEstablishedText(this.itemId).subscribe(text => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(text, 'text/html');
+      const images: any = xmlDoc.querySelectorAll('img.est_figure_graphic');
+      for (let i = 0; i < images.length ; i++) {
+        let image = images[i].src;
+        image = image.replace(`${window.location.origin}/images/verk/`, '');
+        image = 'http://api.sls.fi/digitaledition/topelius/gallery/get/19/' + image;
+        this.images.push(image);
+      }
+    });
   }
-  // ionViewWillLoad() {
-  //   if (this.navParams.get('galleryPage') !== undefined) {
-  //     console.log(this.navParams.get('galleryPage'), 'blääää');
-  //     const page = this.config.getSettings('galleryImages.' + this.navParams.get('galleryPage'));
-  //     this.imgPath = this.illustrationsPath + page.prefix + this.navParams.get('imageNumber') + '.jpg';
-  //   } else {
-  //     this.imgPath = this.illustrationsPath + 'FFiT_' + this.navParams.get('imageNumber') + '.jpg';
-  //   }
-  // }
 }
