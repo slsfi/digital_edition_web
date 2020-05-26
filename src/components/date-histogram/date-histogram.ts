f7a602368969eb73b8ca5e248bcad3bee5188d1e
SLS\niklil <niklas.liljestrand@sls.fi>
2020-05-26 18:33:10 +0300

    fix lint issues

--- src/components/date-histogram/date-histogram.ts
+++ src/components/date-histogram/date-histogram.ts
@@ -21,36 +21,33 @@ export class DateHistogram {
   max = 0
 
   ngOnInit() {
-    console.log('init', this.years)
-    this.updateMax()
+    this.updateMax();
   }
 
   ngOnChanges() {
-    this.updateMax()
+    this.updateMax();
   }
 
   private updateMax() {
     this.max = this.years.reduce(function (current, year) {
-      return Math.max(current, year.doc_count)
+      return Math.max(current, year.doc_count);
     }, 0)
   }
 
   reset() {
-    this.from = null
-    this.to = null
-    this.change(null, null)
+    this.from = null;
+    this.to = null;
+    this.change(null, null);
   }
 
   // Send the change event to the parent component
   onChange() {
     const fromTime = new Date(this.from).getTime()
     // Add one year to get the full year.
-    const toTime = new Date(`${parseInt(this.to) + 1}`).getTime()
-
-    console.log(fromTime, toTime)
+    const toTime = new Date(`${parseInt(this.to) + 1}`).getTime();
 
     if (fromTime <= toTime) {
-      this.change(fromTime, toTime)
+      this.change(fromTime, toTime);
     }
     // this.change(
     //   this.years.find(year => year.key_as_string === this.from),
@@ -68,27 +65,27 @@ export class DateHistogram {
 
   selectYear(selected: Facet) {
     if (!this.from) {
-      this.from = selected.key_as_string
+      this.from = selected.key_as_string;
     } else if (!this.to && parseInt(selected.key_as_string) >= parseInt(this.from)) {
-      this.to = selected.key_as_string
+      this.to = selected.key_as_string;
     } else {
-      this.from = selected.key_as_string
-      this.to = null
+      this.from = selected.key_as_string;
+      this.to = null;
     }
 
-    this.onChange()
+    this.onChange();
   }
 
   isYearInRange(year: Facet) {
     const yearNumber = parseInt(year.key_as_string)
     if (year.key_as_string === this.from || year.key_as_string === this.to) {
-      return true
+      return true;
 
     } else if (this.from && this.to) {
-      return yearNumber >= parseInt(this.from) && yearNumber <= parseInt(this.to)
+      return yearNumber >= parseInt(this.from) && yearNumber <= parseInt(this.to);
 
     } else {
-      return false
+      return false;
     }
   }
 }
