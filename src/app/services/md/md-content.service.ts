c3efa3e13ac5c31c43c2bb544d0f9b40a0402a83
Christoffer Björkskog <christoffer.bjorkskog@genero.fi>
2020-05-27 07:35:35 +0300

    prevent lint errors

--- src/app/services/md/md-content.service.ts
+++ src/app/services/md/md-content.service.ts
@@ -65,15 +65,22 @@ export class MdContentService {
     }
   }
 
-  private getNodeById(id, node) {
-    const reduce = [].reduce;
-    function runner(result, rnode) {
-        if (result || !rnode) { return result; }
-        return rnode.id === id && rnode ||
-            runner(null, rnode.children) ||
-            reduce.call(Object(rnode), runner, result);
+  /**
+   * Find a node by id in a JSON tree
+   */
+  getNodeById(id, node) {
+    try {
+      return this.runner(null, node);
+    } catch (e) {
+      console.log('error finding node by id (in md-content.service)', id, node);
     }
-    return runner(null, node);
+  }
+
+  private runner(result, rnode) {
+    if (result || !rnode) { return result; }
+    return rnode.id === id && rnode ||
+        this.runner(null, rnode.children) ||
+        [].reduce.call(Object(rnode), this.runner, result);
   }
 
   private extractData(res: Response) {
