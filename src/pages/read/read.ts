d464c8e7436413e33bcbe7bb469049c565d6949a
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2021-02-12 10:06:55 +0200

    Adjust tooltip timeout

--- src/pages/read/read.ts
+++ src/pages/read/read.ts
@@ -995,7 +995,7 @@ export class ReadPage /*implements OnDestroy*/ {
   hideToolTip() {
     window['reload_timer'] = setTimeout(() => {
       this.showToolTip = false;
-    }, 5000);
+    }, 500);
   }
 
   showFootnoteTooltip(id: string, origin: any) {
