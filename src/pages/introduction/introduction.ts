575824416a17dab69958b7a8cd304c3ed45fee60
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2021-03-25 08:47:17 +0200

    Get read popover toggle settings from config

--- src/pages/introduction/introduction.ts
+++ src/pages/introduction/introduction.ts
@@ -933,18 +933,25 @@ export class IntroductionPage {
   }
 
   showPopover(myEvent) {
-    const toggles = {
-      'comments': false,
-      'personInfo': true,
-      'placeInfo': false,
-      'workInfo': true,
-      'changes': false,
-      'normalisations': false,
-      'abbreviations': false,
-      'pageNumbering': true,
-      'pageBreakOriginal': false,
-      'pageBreakEdition': true
-    };
+    let toggles = undefined;
+    try {
+      toggles = this.config.getSettings('settings.introToggles');
+    } catch (e) {
+    }
+    if (toggles === undefined || toggles === null) {
+      toggles = {
+        'comments': false,
+        'personInfo': false,
+        'placeInfo': false,
+        'workInfo': false,
+        'changes': false,
+        'normalisations': false,
+        'abbreviations': false,
+        'pageNumbering': true,
+        'pageBreakOriginal': false,
+        'pageBreakEdition': false
+      };
+    }
     const popover = this.popoverCtrl.create(ReadPopoverPage, {toggles}, { cssClass: 'popover_settings' });
     popover.present({
       ev: myEvent
