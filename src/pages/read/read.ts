7195c9be442e150877fcb2d722be71b9d52186ad
SLS\niklil <niklas.liljestrand@sls.fi>
2021-04-13 08:40:19 +0300

    fix read-text links redirecting to same page

--- src/pages/read/read.ts
+++ src/pages/read/read.ts
@@ -963,6 +963,9 @@ export class ReadPage /*implements OnDestroy*/ {
       eventTarget = event.target as HTMLElement;
       if (eventTarget.classList.length === 0 || !eventTarget.classList.contains('xreference')) {
         eventTarget = eventTarget.parentElement;
+        if (eventTarget.classList.length === 0 || !eventTarget.classList.contains('xreference')) {
+          eventTarget = eventTarget.parentElement;
+        }
       }
 
       if (eventTarget.classList.length !== 0 &&
@@ -1037,33 +1040,38 @@ export class ReadPage /*implements OnDestroy*/ {
           targetId = anchorElem.parentElement.getAttribute('href');
         }
 
-        const targetParts = String(targetId).split('#');
-        const hrefTargetItems: Array<string> = String(targetParts[0]).split('_');
-        let collectionId = '';
-        let publicationId = '';
-        const positionId = (targetParts.length > 1) ? targetParts[1] : null;
+        const targetParts = String(decodeURIComponent(targetId)).split('#');
+        const elementInPage: NodeListOf<HTMLElement> = document.getElementsByName(targetParts[1]);
+        if ( elementInPage.length > 0 ) {
+          this.scrollToHTMLElement(elementInPage[0], 'top');
+        } else {
+          const hrefTargetItems: Array<string> = String(targetParts[0]).split(' ');
+          let collectionId = '';
+          let publicationId = '';
+          const positionId = (targetParts.length > 1) ? targetParts[1] : null;
 
-        // Link to reading text or comment. Remove URL encoded blanks and trim the string
-        collectionId =  String(hrefTargetItems[0]).replace('%20', '').trim();
-        publicationId = String(hrefTargetItems[1]).replace('%20', '').trim();
-        if ( collectionId !== '' && publicationId !== '' ) {
-          /* Check if we are usring legacy_ids for the reading text link */
-          this.textService.getCollectionAndPublicationByLegacyId(collectionId + '_' + publicationId).subscribe(data => {
-            if (data[0] !== undefined) {
-              collectionId = data[0]['coll_id'];
-              publicationId = data[0]['pub_id'];
-            }
-            // Open the postion in a new window
-            let hrefString = '#/publication/' + collectionId + '/text/' + publicationId + '';
-            if ( positionId !== null ) {
-              hrefString += '/nochapter;' + positionId;
-            } else {
-              hrefString += '/nochapter';
-            }
-            hrefString += '/not/infinite/nosong/searchtitle/established&comments';
-            // open the link in a new window/tab
-            window.open(hrefString, '_blank');
-          });
+          // Link to reading text or comment. Remove URL encoded blanks and trim the string
+          collectionId =  String(hrefTargetItems[0]).replace('%20', '').trim();
+          publicationId = String(hrefTargetItems[1]).replace('%20', '').trim();
+          if ( collectionId !== '' && publicationId !== '' ) {
+            /* Check if we are usring legacy_ids for the reading text link */
+            this.textService.getCollectionAndPublicationByLegacyId(collectionId + '_' + publicationId).subscribe(data => {
+              if (data[0] !== undefined) {
+                collectionId = data[0]['coll_id'];
+                publicationId = data[0]['pub_id'];
+              }
+              // Open the postion in a new window
+              let hrefString = '#/publication/' + collectionId + '/text/' + publicationId + '';
+              if ( positionId !== null ) {
+                hrefString += '/nochapter;' + positionId;
+              } else {
+                hrefString += '/nochapter';
+              }
+              hrefString += '/not/infinite/nosong/searchtitle/established&comments';
+              // open the link in a new window/tab
+              window.open(hrefString, '_blank');
+            });
+          }
         }
       }
     }).bind(this);
