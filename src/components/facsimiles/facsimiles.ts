4e9e64a6d47f6abd1ce2e2caf9e08f60991e7986
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2021-05-11 15:19:10 +0300

    Update facsimiles.ts

--- src/components/facsimiles/facsimiles.ts
+++ src/components/facsimiles/facsimiles.ts
@@ -355,6 +355,7 @@ export class FacsimilesComponent {
 
       console.log(this.selectedFacsimile.publication_facsimile_collection_id + ' | ' + facsimile.publication_facsimile_collection_id);
       console.log(this.selectedFacsimile.page + ' | ' + facsimile.page);
+      console.log(this.selectedFacsimile.page_nr + ' | ' + facsimile.page_nr);
 
       if (this.selectedFacsimile.publication_facsimile_collection_id === facsimile.publication_facsimile_collection_id
       && this.selectedFacsimile.page !== undefined && this.selectedFacsimile.page === facsimile.page) {
