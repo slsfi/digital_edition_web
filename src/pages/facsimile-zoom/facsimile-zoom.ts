223c064e0adaa69c3fff1bb7f8c33c33da0679ff
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2021-06-02 12:26:25 +0300

    Update facsimile-zoom.ts

--- src/pages/facsimile-zoom/facsimile-zoom.ts
+++ src/pages/facsimile-zoom/facsimile-zoom.ts
@@ -93,7 +93,7 @@ export class FacsimileZoomModalPage {
       }
 
       /* Append dot to title */
-      if (this.imageTitles.length > 0) {
+      if (this.imageTitles !== undefined && this.imageTitles.length > 0) {
         for (let x = 0; x < this.imageTitles.length; x++) {
           if (this.imageTitles[x] !== '' && this.imageTitles[x] !== 'null'
           && this.imageTitles[x] !== undefined && this.imageTitles[x] !== null) {
