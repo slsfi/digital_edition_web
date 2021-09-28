e586d277b3ae9b31792ee3a7f39aeb9e6a7a85a1
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2021-09-28 11:23:52 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1016,7 +1016,6 @@ export class DigitalEditionsApp {
 
       const params = {};
       const nav = this.app.getActiveNavs();
-      /*this.openPage('HomePage');*/
       nav[0].setRoot('HomePage', params, { animate: false });
       nav[0].goToRoot();
     });
