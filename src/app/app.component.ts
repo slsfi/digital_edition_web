2d2aed9325ae8bfacb1353c3c7f133493ebd55d3
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2021-09-28 10:36:25 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1016,9 +1016,9 @@ export class DigitalEditionsApp {
 
       const params = {};
       const nav = this.app.getActiveNavs();
-      this.openPage('HomePage');
-      nav[0].setRoot('HomePage', params, { animate: false });
       this.enableTableOfContentsMenu();
+      nav[0].setRoot('HomePage', params, { animate: false });
+      
     });
 
     this.events.subscribe('DigitalEditionList:recieveData', (data) => {
