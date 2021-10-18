a81dd451ee48fac03d0c476ad1fd2cdaf024b103
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2021-10-18 21:59:56 +0300

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -76,7 +76,8 @@ export class HomePage {
     try {
       this.imageUrl = this.config.getSettings('frontpageConfig.imageUrl');
     } catch (e) {
-      this.imageUrl = 'assets/images/frontpage-image-portrait.jpg';
+      // this.imageUrl = 'assets/images/frontpage-image-portrait.jpg';
+      this.imageUrl = 'https://granska-topelius.sls.fi/assets/images/frontpage-image-portrait.jpg';
     }
     if (this.userSettingsService.isMobile()) {
       if (this.imageOrientationPortrait) {
