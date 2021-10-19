d24fa30de4cbf6be20a028b8753007f85a42c116
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2021-10-19 13:12:18 +0300

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -52,7 +52,7 @@ export class HomePage {
     try {
       this.imageOrientationPortrait = this.config.getSettings('frontpageConfig.imageOrientationIsPortrait');
     } catch (e) {
-      this.imageOrientationPortrait = true;
+      this.imageOrientationPortrait = false;
     }
     try {
       this.imageOnRight = this.config.getSettings('frontpageConfig.imageOnRightInPortrait');
