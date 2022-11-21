d987af2b43a2491050f9d4d25a6d9b97cd852fe8
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2022-11-21 15:39:45 +0200

    Update elastic-search.ts

--- src/pages/elastic-search/elastic-search.ts
+++ src/pages/elastic-search/elastic-search.ts
@@ -420,7 +420,8 @@ export class ElasticSearchPage {
       queries: this.queries,
       highlight: {
         fields: {
-          "textDataIndexed": { number_of_fragments: 1000, fragment_size: 150, type: "plain" }
+          "textDataIndexed": { number_of_fragments: 1000, fragment_size: 150, type: "plain" },
+          "publication_data.pubname": { number_of_fragments: 0, type: "plain" },
         },
       },
       from: this.from,
